20cb87f841f92906215b169c8052965801e911d0
Albert Corominas <albertcorominasmariscot@gmail.com>
2024-04-24 22:12:10 +0200

    More Challenges

---
+++ matchstick_houses.js
@@ -0,0 +1,25 @@
+// Matchstick Houses
+
+/*
+    This challenge will help you interpret mathematical relationships both algebraically and geometrically.
+
+    Matchstick Houses, Steps 1, 2 and 3
+
+    Create a function that takes a number (step) as an argument and returns the number of matchsticks in that step. 
+    See step 1, 2 and 3 in the image above. Image: https://edabit-challenges.s3.amazonaws.com/matchstick_houses.png
+
+    Examples
+    matchHouses(1) ➞ 6
+    matchHouses(4) ➞ 21
+    matchHouses(87) ➞ 436
+
+    Notes:
+    Step 0 returns 0 matchsticks.
+    The input (step) will always be a non-negative integer.
+    Think of the input (step) as the total number of houses that have been connected together.
+*/
+
+function matchHouses(step) {
+    if(step === 0) return 0;
+    return 5*step + 1;
+}
